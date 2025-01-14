1fe1b8c1cdb842484fc6055e11c2a2cb1d4f6b48
Frederick-Tejeda <103900166+Frederick-Tejeda@users.noreply.github.com>
2025-01-13 22:19:58 -0400

    Control de excepciones

---
+++ Manejo_de_errores/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Manejo_de_errores{
+    class Program{
+        static void Main(string[] args){
+            try{
+                string content = File.ReadAllText("./Ejemplo.txt");
+                Console.WriteLine(content);
+            }
+            catch(FileNotFoundException ex){
+                Console.WriteLine("Hubo un problema");
+                Console.WriteLine("Asegurate de que el archivo este nombreado correctamente \"Ejemplo.txt\"");
+            }
+            catch(DirectoryNotFoundException ex){
+                Console.WriteLine("Hubo un problema");
+                Console.WriteLine("Asegurate de que el archivo este ubicado en la ruta raiz \"Ejemplo.txt\"");
+            }
+            catch(Exception ex){
+                Console.WriteLine("Algo salio mal...");
+                Console.WriteLine(ex.Message);
+            }
+            finally{
+                Console.WriteLine("Cerrando la aplicacion...");
+            }
+        }
+    }
+}
