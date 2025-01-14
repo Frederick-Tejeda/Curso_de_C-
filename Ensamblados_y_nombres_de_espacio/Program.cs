f9687957088fd1b810b2d6d7197c6396cbdd83ab
Frederick-Tejeda <103900166+Frederick-Tejeda@users.noreply.github.com>
2025-01-14 00:28:00 -0400

    Ensamblados_y_nombres_de_espacio

---
+++ Ensamblados_y_nombres_de_espacio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Net;
+namespace Ensamblados_y_nombres_de_espacio{
+    class Program{
+        static void Main(string[] args){
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            File.WriteAllText("./WriteText.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
