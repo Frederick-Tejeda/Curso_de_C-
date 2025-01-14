65765d3773b4d67c2d9712b9c81f28225c2b264b
Frederick-Tejeda <103900166+Frederick-Tejeda@users.noreply.github.com>
2025-01-14 01:04:06 -0400

    Referencias_a_ensamblados

---
+++ Referencias_a_ensamblados/clase/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace clase;
+
+public class Scrape
+{
+    public string ScrapeWebPage(string url){
+        return GetWebPage(url);
+    }
+    public string ScrapeWebPage(string url, string filepath){
+        string reply =  GetWebPage(url);
+        File.WriteAllText(filepath, reply);
+        return reply;
+    }
+    public string GetWebPage(string url){
+        WebClient client = new WebClient();
+        return client.DownloadString(url);
+    }
+}
---
+++ Referencias_a_ensamblados/cliente/Program.cs
@@ -0,0 +1,12 @@
+﻿using clase;
+
+namespace cliente{
+    class Program{
+        static void Main(string[] args){
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebPage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
